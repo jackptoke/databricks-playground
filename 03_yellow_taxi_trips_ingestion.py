1ab3b4a9da8993468928e844e0a1ebe7b67bf892
Jack Toke <jack.toke@karieng.com.au>
2026-06-10 15:58:20 +1000

    Done

--- 03_yellow_taxi_trips_ingestion.py
+++ 03_yellow_taxi_trips_ingestion.py
@@ -12,25 +12,13 @@ first_day_next_month = get_first_day_of_next_month(month)
 dir_path = f"/Volumes/nyctaxi/landing/data_sources/nyctaxi_yellow/trips/{month}/"
 
 try:
-    count_df = spark.sql("""
-    SELECT COUNT(*) AS count FROM nyctaxi.landing.trips_raw
-    WHERE tpep_pickup_datetime >= :first_day AND tpep_pickup_datetime < :first_day_next_month
-    """, args={
-        "first_day": first_day.strftime("%Y-%m-%d"),
-        "first_day_next_month": first_day_next_month.strftime("%Y-%m-%d")
-        })
-
-    # If data exists, skip
-    if count_df.first()["count"] > 0:
-        print(f"Data for {month} already exists")
-    else:
-        spark.sql(f"""
-        COPY INTO nyctaxi.landing.trips_raw
-        FROM '{dir_path}'
-        FILEFORMAT = PARQUET
-        FORMAT_OPTIONS ('recursiveFileLookup' = 'true');
-        """)
-        print("Raw data extracted successfully.")
+    spark.sql(f"""
+    COPY INTO nyctaxi.landing.trips_raw
+    FROM '{dir_path}'
+    FILEFORMAT = PARQUET
+    FORMAT_OPTIONS ('recursiveFileLookup' = 'true');
+    """)
+    print("Raw data extracted successfully.")
 except Exception as e:
     print(f"Error: {e}")
     raise
